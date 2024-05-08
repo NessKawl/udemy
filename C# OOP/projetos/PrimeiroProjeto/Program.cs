cbb668823aec89e2fc5edd61e14829bca2a04b86
Marcos Almeida <80133181+NessKawl@users.noreply.github.com>
2024-05-07 22:04:54 -0300

    07/05/2024

---
+++ C# OOP/projetos/PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // referência para o namespace padrão System, é um namespace padrão do .NET (contém funções básicas de manipulação de programas)
+
+namespace PrimeiroProjeto // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args) // identificação padrão do C# para identificar o entry point da aplicação (onde a execução começa)
+        {
+            Console.WriteLine("Hello World!"); // o comando Console.WriteLine pertence ao namespace System, 
+        }
+    }
+}
