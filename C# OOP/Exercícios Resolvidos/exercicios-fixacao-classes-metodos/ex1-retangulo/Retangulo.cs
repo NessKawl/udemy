d39f360b87752729435bf4a276fc4a0fedbe19d7
Marcos Almeida <80133181+NessKawl@users.noreply.github.com>
2024-08-22 10:53:55 -0300

    Exercícios de fixação de classes e métodos
    - Resolução dos três exercícios de fixação sobre classes e métodos;

---
+++ C# OOP/Exercícios Resolvidos/exercicios-fixacao-classes-metodos/ex1-retangulo/Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex1_retangulo
+{
+    internal class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double Area()
+        {
+            return Altura * Largura;
+        }
+
+        public double Perimetro()
+        {
+            return (Largura * 2) + (Altura * 2); 
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt((Largura * Largura) + (Altura * Altura));
+        }
+    }
+}
