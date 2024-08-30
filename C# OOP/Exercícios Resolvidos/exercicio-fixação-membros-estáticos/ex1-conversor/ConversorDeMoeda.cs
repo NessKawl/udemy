c8735ea7eeeaa1b9cfae8c66c821d9b5193fadeb
Marcos Almeida <80133181+NessKawl@users.noreply.github.com>
2024-08-30 11:37:53 -0300

    Exercícios de fixação membros estáticos + projeto gerenciador de estoque
    - Resolução do exercício de membros estáticos;
    - Inicialização do projeto GerenciadorEstoque;

---
+++ C# OOP/Exercícios Resolvidos/exercicio-fixação-membros-estáticos/ex1-conversor/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ex1_conversor
+{
+    internal class ConversorDeMoeda
+    {
+        public double Cotacao;
+        public double ComprarDolar;
+
+        public double ValorEmReais ()
+        {
+            double imposto = (Cotacao * ComprarDolar) * 0.06;
+            return Cotacao * ComprarDolar + imposto;
+        }
+
+    }
+}
---
+++ C# OOP/projetos/GerenciadorEstoque/Produto.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace GerenciadorEstoque
+{
+    internal class Produto
+    {
+
+        public string NomeProduto = "";
+        public string DescProduto = "";
+        public double PrecoProduto;
+        public double QtdProduto;
+    
+
+    }
+}
